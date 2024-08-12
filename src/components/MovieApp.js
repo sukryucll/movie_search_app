354c81a62a4cafa6ea5f8a4afec7ca8d9a18a374
sukryucll <sukr.yucll.02@gmail.com>
2024-08-12 14:44:02 +0300

    v2_1

--- src/components/MovieApp.js
+++ src/components/MovieApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./MovieApp.css";
 import { AiOutlineSearch } from "react-icons/ai";
 import axios from "axios";
+
 function MovieApp() {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
@@ -21,7 +22,6 @@ function MovieApp() {
         }
       );
       setGenres(response.data.genres);
-      console.log(response.data.genres)
     };
     fetchGenres();
   }, []);
@@ -33,7 +33,7 @@ function MovieApp() {
         {
           params: {
             api_key: "014a9a37e6b52563a89dedcb069f9772",
-            sortBy: sortBy,
+            sort_by: sortBy, // Düzeltilmiş parametre adı
             page: 1,
             with_genres: selectedGenre,
             query: searchQuery,
@@ -65,6 +65,7 @@ function MovieApp() {
   const toggleDescription = (movieId) => {
     setExpandedMovieId(expandedMovieId === movieId ? null : movieId);
   };
+
   const handleSortChange = (event) => {
     setSortBy(event.target.value);
   };
@@ -72,9 +73,10 @@ function MovieApp() {
   const handleGenreChange = (event) => {
     setSelectedGenre(event.target.value);
   };
+
   return (
     <div>
-      <h1>movieApp</h1>
+      <h1>MovieApp</h1>
       <div className="search-bar">
         <input
           type="text"
@@ -82,7 +84,7 @@ function MovieApp() {
           value={searchQuery}
           onChange={handleSearchChange}
           className="search-input"
-        ></input>
+        />
         <button onClick={handleSearchSubmit} className="search-button">
           <AiOutlineSearch />
         </button>
@@ -93,10 +95,10 @@ function MovieApp() {
         <select id="sort-by" value={sortBy} onChange={handleSortChange}>
           <option value="popularity.desc">Popularity Descending</option>
           <option value="popularity.asc">Popularity Ascending</option>
-          <option value="vote_average.asc">Rating Descending</option>
+          <option value="vote_average.desc">Rating Descending</option>
           <option value="vote_average.asc">Rating Ascending</option>
-          <option value="release_date.desc">Release Date Descending </option>
-          <option value="release_date.asc">Release Date Ascending </option>
+          <option value="release_date.desc">Release Date Descending</option>
+          <option value="release_date.asc">Release Date Ascending</option>
         </select>
         <label htmlFor="genre">Genre:</label>
         <select id="genre" value={selectedGenre} onChange={handleGenreChange}>
@@ -108,6 +110,7 @@ function MovieApp() {
           ))}
         </select>
       </div>
+
       <div className="movie-wrapper">
         {movies.map((movie) => (
           <div key={movie.id} className="movie">
@@ -126,7 +129,7 @@ function MovieApp() {
               className="read-more"
               onClick={() => toggleDescription(movie.id)}
             >
-              {expandedMovieId === movie.id ? "Show less" : "read more"}{" "}
+              {expandedMovieId === movie.id ? "Show less" : "Read more"}
             </button>
           </div>
         ))}
